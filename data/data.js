3126c3be5d4e6e2600b951cd0111fdafb42edbb0
Taufiq <dhanani94@gmail.com>
2016-06-29 12:42:40 -0400

    imaage done

--- data/data.js
+++ data/data.js
@@ -1,6 +1,8 @@
 var data = {};
 
 data.group1 = [
+	{name : "Taufiq Bha", ref : "TaufiqBha"},
+	{name : "Zoya Bai", ref : "Zoya Bai"}
 	{name : "Neha Momin", ref : "NehaMomin"},
 	{name : "Shayna Husein", ref : "ShaynaHusein"},
 	{name : "Mishaal Gilani", ref : "MishaalGilani"},
@@ -9,6 +11,7 @@ data.group1 = [
 	{name : "Sara Maherali", ref : "SaraMaherali"},
 	{name : "Manisha Dhanani", ref : "ManishaDhanani"},
 	{name : "Faiz Shamji", ref : "FaizShamji"},
+	{name : "Kiran Bai", ref : "KiranBai"},
 	{name : "Aahil Madhani", ref : "AahilMadhani"},
 	{name : "Kiaan Bharwani", ref : "KiaanBharwani"},
 	{name : "Saahir Hussain", ref : "SaahirHussain"},
@@ -18,19 +21,18 @@ data.group1 = [
 	{name : "Wasim Meghani", ref : "WasimMeghani"},
 	{name : "Alishan Ali", ref : "AlishanAli"},
 	{name : "Aliza Ali", ref : "AlizaAli"},
-	{name : "Taufiq Bha", ref : "TaufiqBha"},
-	{name : "Kiran Bai", ref : "KiranBai"},
 	{name : "Sahil Bha", ref : "SahilBha"},
-	{name : "Zoya Bai", ref : "Zoya Bai"}
 ];
 
 data.group2 = [
+	{name : "Taufiq Bha", ref : "TaufiqBha"},
 	{name: "Anusha Jiwani", ref : "AnushaJiwani"},
 	{name: "Innara Basria", ref : "InnaraBasria"},
 	{name: "Anmol Momin", ref : "AnmolMomin"},
 	{name: "Inaara Panjwani", ref : "InaaraPanjwani"},
 	{name: "Alisha Hooda", ref : "AlishaHooda"},
 	{name: "Shaheen Pirani", ref : "ShaheenPirani"},
+	{name : "Farhana Bai", ref : "FarhanaBai"},
 	{name: "Shumaila Panjwani", ref : "ShumailaPanjwani"},
 	{name: "Arzoo Virani", ref : "ArzooVirani"},
 	{name: "Faraz Ali", ref : "FarazAli"},
@@ -41,10 +43,8 @@ data.group2 = [
 	{name: "Arsh Momin", ref : "ArshMomin"},
 	{name: "Rahil Patel", ref : "RahilPatel"},
 	{name: "Zaryab Prasla", ref : "ZaryabPrasla"},
-	{name : "Taufiq Bha", ref : "TaufiqBha"},
-	{name : "Zoya Bai", ref : "Zoya Bai"},
-	{name : "Farhana Bai", ref : "FarhanaBai"},
-	{name : "Senior Staff", ref : "SeniorStaff"}
+	{name : "Zoya Bai", ref : "Zoya Bai"}
+	// {name : "Senior Staff", ref : "SeniorStaff"}
 ]
 
 module.exports = data;
